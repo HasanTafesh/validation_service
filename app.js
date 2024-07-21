3a6a94e8b9e69c88e910605f93a0c67e6aa51562
hasan tafesh <71045914+HasanTafesh@users.noreply.github.com>
2024-07-21 16:21:53 +0300

    initial commit

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerDocument = require('./swagger.json');
 const validationRoutes = require('./routes/validationRoutes');
 const badWordRoutes = require('./routes/badWordRoutes');
 const celebrityNameRoutes = require('./routes/celebrityNameRoutes');
@@ -9,6 +12,15 @@ const { loadAllToCache } = require('./services/cacheService');
 
 const app = express();
 app.use(bodyParser.json());
+
+const swaggerOptions = {
+  swaggerDefinition: swaggerDocument,
+  apis: ['./routes/*.js'],
+};
+
+const swaggerSpecs = swaggerJsdoc(swaggerOptions);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
 app.use('/api/validation', validationRoutes);
 app.use('/api/badwords', badWordRoutes);
--- routes/validationRoutes.js
+++ routes/validationRoutes.js
@@ -1,3 +1,64 @@
+/**
+ * @swagger
+ * /api/validation/validate:
+ *   post:
+ *     summary: Validate input data
+ *     description: Validates the provided email, phone number, and full name
+ *     parameters:
+ *       - in: body
+ *         name: body
+ *         required: true
+ *         schema:
+ *           type: object
+ *           properties:
+ *             email:
+ *               type: string
+ *             phone:
+ *               type: string
+ *             firstName:
+ *               type: string
+ *             lastName:
+ *               type: string
+ *           required:
+ *             - email
+ *             - phone
+ *             - firstName
+ *             - lastName
+ *     responses:
+ *       200:
+ *         description: Validation result
+ *         schema:
+ *           type: object
+ *           properties:
+ *             email:
+ *               type: object
+ *               properties:
+ *                 email:
+ *                   type: string
+ *                 valid:
+ *                   type: boolean
+ *                 reason:
+ *                   type: string
+ *             phone:
+ *               type: object
+ *               properties:
+ *                 phone:
+ *                   type: string
+ *                 valid:
+ *                   type: boolean
+ *             fullName:
+ *               type: object
+ *               properties:
+ *                 firstName:
+ *                   type: string
+ *                 lastName:
+ *                   type: string
+ *                 valid:
+ *                   type: boolean
+ *                 reason:
+ *                   type: string
+ */
+
 const express = require('express');
 const { validate } = require('../controllers/validationController');
 const checkApiKey = require('../middlewares/authMiddleware');
